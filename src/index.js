078e2508a695e056163ee7c80cb4a9f6d5fee08d
JungUk <62638035+JungUk@users.noreply.github.com>
2022-09-14 18:36:31 +0900

    두뇌팡팡

--- src/index.js
+++ src/index.js
@@ -23,13 +23,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // 왜 slice() 함수를 쓰는 건가? 안써도 오류는 없는데. -> 사본을 만들어 불변성을 
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -42,7 +46,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner : ' + winner;
+    }
+    else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -81,6 +92,38 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    // 배열 하나마다 승리 조건
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // 여기까지는 보기 편하게 주석으로 끊었음
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4 ,6],
+  ];
+
+  for(let i = 0; i < lines.length; i++) {
+    const[a, b, c] = lines[i];
+    // const a = lines[i, 0];
+    // const b = lines[i, 1];
+    // const c = lines[i, 2];
+
+    // 밑에 squares[a] === squares[b] && squares[a] === squares[c] 로 하면 
+    // 승리했다고 몇턴 지나서 뜨는데 왜 그런거지?
+    // a=b 이고 a=c 이면 b=c 다
+    // 역으로 b=c이면 b=a 이고 a=c 이어야 하는건데
+    // b=c라고 해서 b=a 가 맞는지는 알 수 없다
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
